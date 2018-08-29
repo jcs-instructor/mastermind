eaee4a3b495e9bdf9995626e03026e6428d651d3
jcs-instructor <jcsilberman@gmail.com>
2018-08-29 15:14:59 -0400

    Created test environment

---
+++ src/test/MastermindTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MastermindTest {
+
+    @Test
+    void firstTest() {
+        assertEquals(1,1);
+    }
+}
